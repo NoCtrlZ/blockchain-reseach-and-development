ec30d16e366ac36b932646f739b3a81945c1129b
Hemlocked <phantomofrotten@gmail.com>
2020-02-10 14:10:30 +0900

    listen server socket

--- p2p/src/main.rs
+++ p2p/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+pub fn main() {
+    let endpoint = "127.0.0.1:9000";
+    let listener = TcpListener::bind(&endpoint).unwrap();
+    for stream in listener.incoming() {
+        println!("Server is listening on {}", endpoint);
+    }
 }
