14246f1e88fc69ca1e238a362b1c7d71205b63b6
NoCtrlZ <phantomofrotten@gmail.com>
2020-03-03 14:18:50 +0900

    initialize block

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 mod core;
+mod unit;
 use std::any::type_name;
-use crypto::sha2::Sha256;
-use crypto::digest::Digest;
 
 fn type_of<T>(_: T) -> &'static str {
     type_name::<T>()
 }
 
 fn main() {
-    let mut sha256 = Sha256::new();
-    sha256.input_str("Hi Sexy");
-    println!("{:?}", type_of(&sha256.result_str()));
+    let mut blockchain = core::Blockchain::new();
+    blockchain.print_latest_block();
 }
