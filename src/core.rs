c729fe4652bac28fb5521acbe0e4ce42a40598fc
NoCtrlZ <phantomofrotten@gmail.com>
2020-03-03 13:38:03 +0900

    hash function and data structure

--- src/core.rs
+++ src/core.rs
@@ -1,13 +1,17 @@
-use std::time::SystemTime;
-
-pub struct Blockchain {
-    pub entity: Vec<Block>
+pub struct Blockchain<'a> {
+    pub entity: Vec<Block<'a>>
 }
 
-struct Block {
+pub struct Block<'a> {
     index: u32,
     timestamp: u64,
-    transactions: Vec<Transaction>,
-    nonce: u64
+    transactions: Vec<Transaction<'a>>,
+    nonce: u64,
+    hash: &'a str
 }
 
+pub struct Transaction<'a> {
+    amount: u64,
+    sender: &'a str,
+    recipient: &'a str
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-// mod core;
-use std::time::{SystemTime, UNIX_EPOCH};
+mod core;
+use std::any::type_name;
+use crypto::sha2::Sha256;
+use crypto::digest::Digest;
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
 
 fn main() {
-    let now = SystemTime::now();
-    let unixtime = now.duration_since(UNIX_EPOCH).expect("invalid time");
-    println!("{:?}", unixtime.as_secs().type_name_of());
+    let mut sha256 = Sha256::new();
+    sha256.input_str("Hi Sexy");
+    println!("{:?}", type_of(&sha256.result_str()));
 }
