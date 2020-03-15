18d51604cbcc449a8f97f05f0fdfa58cf9652124
NoCtrlZ <phantomofrotten@gmail.com>
2020-03-15 22:42:19 +0900

    generate random port number

--- p2p/client/src/main.rs
+++ p2p/client/src/main.rs
@@ -3,7 +3,7 @@ use hyper::{body::HttpBody as _, Client};
 #[tokio::main]
 async fn main() {
     let client = Client::new();
-    let uri = "http://localhost:3000".parse().unwrap();
+    let uri = "http://127.0.0.1:3000".parse().unwrap();
     let resp = client.get(uri).await.unwrap();
     println!("Response: {}", resp.status());
 }
--- p2p/server/src/main.rs
+++ p2p/server/src/main.rs
@@ -1,7 +1,9 @@
+extern crate rand;
 use std::convert::Infallible;
 use std::net::SocketAddr;
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
+use rand::Rng;
 
 async fn peer_to_peer(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     match (req.method(), req.uri().path()) {
@@ -18,7 +20,9 @@ async fn peer_to_peer(req: Request<Body>) -> Result<Response<Body>, Infallible> 
 
 #[tokio::main]
 async fn main() {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let mut rng = rand::thread_rng();
+    let addr = SocketAddr::from(([127, 0, 0, 1], rng.gen_range(1024, 9000)));
+    println!("{:?}", addr);
 
     let make_svc = make_service_fn(|_conn| async {
         Ok::<_, Infallible>(service_fn(peer_to_peer))
