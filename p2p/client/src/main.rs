63262dd358f9f9d46173de1aec9c5e9774c90e58
NoCtrlZ <phantomofrotten@gmail.com>
2020-03-15 22:24:48 +0900

    root path set

--- p2p/client/src/main.rs
+++ p2p/client/src/main.rs
@@ -3,7 +3,7 @@ use std::net::TcpStream;
 
 fn main() {
     let message = "here is client";
-    let endpoint = "127.0.0.1:9000";
+    let endpoint = "127.0.0.1:3000";
     let stream = TcpStream::connect(endpoint);
     let mut stream = stream.unwrap();
     stream.write(message.as_bytes()).unwrap();
--- p2p/server/src/main.rs
+++ p2p/server/src/main.rs
@@ -1,10 +1,19 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello, World".into()))
+async fn peer_to_peer(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new(Body::from(
+            "hello world man"
+        ))),
+        _ => {
+            let mut not_found = Response::default();
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
 }
 
 #[tokio::main]
@@ -12,10 +21,9 @@ async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
     let make_svc = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(hello_world))
+        Ok::<_, Infallible>(service_fn(peer_to_peer))
     });
 
-    println!("{:?}", addr);
     let server = Server::bind(&addr).serve(make_svc);
 
     if let Err(e) = server.await {
