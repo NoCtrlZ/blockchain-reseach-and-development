dcc9ce84466b6a6419ee535c88d110f1e64306d9
NoCtrlZ <phantomofrotten@gmail.com>
2020-03-16 13:47:45 +0900

    refactoring p2p

--- integration/src/main.rs
+++ integration/src/main.rs
@@ -5,30 +5,15 @@ mod debug;
 
 extern crate rand;
 use std::convert::Infallible;
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
-use std::net::SocketAddr;
+use hyper::Server;
 use hyper::service::{make_service_fn, service_fn};
-
-pub async fn peer_to_peer(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/") => Ok(Response::new("hello world man".into())),
-        (&Method::POST, "/check_all") => {
-            println!("{:?}", req);
-            Ok(Response::new("hello world man".into()))
-        },
-        _ => {
-            let mut not_found = Response::default();
-            *not_found.status_mut() = StatusCode::NOT_FOUND;
-            Ok(not_found)
-        }
-    }
-}
 
 #[tokio::main]
 async fn main() {
-    let addr = SocketAddr::from(([127, 0, 0, 1], unit::random_port()));
+    let network = p2p::Network::new();
+    let addr = network.get_address();
     let make_svc = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(peer_to_peer))
+        Ok::<_, Infallible>(service_fn(p2p::peer_to_peer))
     });
     let server = Server::bind(&addr).serve(make_svc);
 
--- integration/src/p2p.rs
+++ integration/src/p2p.rs
@@ -1,6 +1,11 @@
+use std::net::SocketAddr;
+use hyper::{Body, Method, Request, Response, StatusCode};
+use std::convert::Infallible;
+
 #[derive(Debug)]
 pub struct Network {
-    pub nodes: Vec<Node>
+    pub nodes: Vec<Node>,
+    pub host: [i32; 4]
 }
 
 #[derive(Debug)]
@@ -8,15 +13,35 @@ pub struct Node {
     port: u16
 }
 
+pub async fn peer_to_peer(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new("hello world man".into())),
+        (&Method::POST, "/check_all") => {
+            println!("{:?}", req);
+            Ok(Response::new("hello world man".into()))
+        },
+        _ => {
+            let mut not_found = Response::default();
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
+}
+
 impl Network {
-    pub fn new(self, port: u16) -> Network {
+    pub fn new() -> Network {
         let mut network = Network {
-            nodes: Vec::new()
+            nodes: Vec::new(),
+            host: [127, 0, 0, 1]
         };
         let original_node = Node {
-            port: port
+            port: 3000
         };
         network.nodes.push(original_node);
         network
+    }
+
+    pub fn get_address(self) -> SocketAddr {
+        SocketAddr::from(([127, 0, 0, 1], self.nodes[0].port))
     }
 }
