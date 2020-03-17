2ecffde9b06f9a1c848985219f3d4828d6635a88
NoCtrlZ <phantomofrotten@gmail.com>
2020-03-17 21:19:02 +0900

    success public key creation

--- lamport-sig/src/key.rs
+++ lamport-sig/src/key.rs
@@ -4,21 +4,34 @@ use crypto::sha2::Sha256;
 use crypto::digest::Digest;
 use std::iter::repeat;
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct PrivateKey {
-    pub pairs: Vec<(U256, U256)>
+    pub pairs: Vec<(U256, U256)>,
+    pub public_key: PublicKey
+}
+
+#[derive(Debug)]
+pub struct PublicKey {
+    pub pairs: Vec<(String, String)>
 }
 
 pub static PRIVATE_KEY_LENGT: usize = 256;
 
 impl PrivateKey {
     pub fn new() -> PrivateKey {
-        let mut pairs = Vec::with_capacity(PRIVATE_KEY_LENGT);
+        let mut prv_pairs = Vec::with_capacity(PRIVATE_KEY_LENGT);
+        let mut pub_pairs = Vec::with_capacity(PRIVATE_KEY_LENGT);
         for _i in 0..PRIVATE_KEY_LENGT {
-            pairs.push(random_uint256_pair());
+            let (adam, eve) = random_uint256_pair();
+            pub_pairs.push(hash_uint256_pair(&adam, &eve));
+            prv_pairs.push((adam, eve));
         }
+        let public_key = PublicKey {
+            pairs: pub_pairs
+        };
         PrivateKey {
-            pairs: pairs
+            pairs: prv_pairs,
+            public_key: public_key
         }
     }
 }
@@ -34,6 +47,10 @@ fn random_uint256() -> U256 {
 fn u64_to_uint256() -> U256 {
     let mut rng = rand::thread_rng();
     (rng.gen::<u64>()).into()
+}
+
+fn hash_uint256_pair(adam: &U256, eve: &U256) -> (String, String) {
+    (sha256_hash(&adam.to_string()), sha256_hash(&eve.to_string()))
 }
 
 fn sha256_hash(random_number: &str) -> String {
