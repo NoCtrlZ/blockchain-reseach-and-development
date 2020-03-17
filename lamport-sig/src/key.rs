38772be18e5dc2f1d85d691b2091fdb4b852816f
NoCtrlZ <phantomofrotten@gmail.com>
2020-03-17 18:42:53 +0900

    uint256 test

--- lamport-sig/src/key.rs
+++ lamport-sig/src/key.rs
@@ -31,3 +31,21 @@ fn u64_to_uint256() -> U256 {
     let mut rng = rand::thread_rng();
     (rng.gen::<u64>()).into()
 }
+
+#[cfg(test)]
+use std::any::type_name;
+
+mod tests {
+    use super::*;
+    use std::any::type_name;
+
+    #[test]
+    fn test_new_block_chain() {
+        let u64_to_uint256 = u64_to_uint256();
+        assert_eq!(type_of(&U256), type_of(&u64_to_uint256));
+    }
+
+    fn type_of<T>(_: T) -> &'static str {
+        type_name::<T>()
+    }
+}
