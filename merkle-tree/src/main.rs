6a29d58ae8447b99bb7f3ff8c5d674a411fec0c5
NoCtrlZ <phantomofrotten@gmail.com>
2020-03-19 15:34:32 +0900

    send transactions method

--- merkle-tree/src/main.rs
+++ merkle-tree/src/main.rs
@@ -4,6 +4,50 @@ struct Tree {
     root: String
 }
 
+#[derive(Debug)]
+struct Transactions {
+    transactions: Vec<Transaction>
+}
+
+#[derive(Debug)]
+struct Transaction {
+    amount: u64,
+    sender: String,
+    recipient: String
+}
+
+impl Tree {
+    fn new(transactions: Vec<String>) -> Tree {
+        Tree {
+            leaves: transactions.clone(),
+            layer: transactions.clone(),
+            root: "".to_string()
+        }
+    }
+}
+
+impl Transactions {
+    fn new() -> Transactions {
+        Transactions {
+            transactions: Vec::new()
+        }
+    }
+
+    fn send_transaction(&mut self, amount: u64, sender: &str, recipient: &str) {
+        self.transactions.push(Transaction {
+            amount: amount,
+            sender: sender.to_string(),
+            recipient: recipient.to_string()
+        })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut transactions = Transactions::new();
+    send_transactions(&mut transactions);
+    println!("{:?}", transactions);
+}
+
+fn send_transactions(transactions: &mut Transactions) {
+    transactions.send_transaction(100, "alice", "bob");
 }
