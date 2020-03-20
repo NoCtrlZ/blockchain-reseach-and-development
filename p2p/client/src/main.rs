67fbea445f5d5d7f948dca104bb7ce269c4356e3
NoCtrlZ <phantomofrotten@gmail.com>
2020-03-20 17:49:22 +0900

    get returned data

--- p2p/client/src/main.rs
+++ p2p/client/src/main.rs
@@ -1,7 +1,7 @@
 use std::io::Write;
 use std::net::TcpStream;
 
-const PREFIX: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: ";
+const PREFIX: &str = "HTTP/1.1\r\nHost: localhost:5862\r\nUser-Agent: curl/7.64.1\r\nAccept: */*";
 
 struct Request {
     method: String,
@@ -18,15 +18,17 @@ fn create_normal_get(path: &str, body: &str) -> String {
     let mut request = method::GET.to_string();
     request.push_str(&format!("{}{}", path, " "));
     request.push_str(PREFIX);
-    request.push_str(&body.len().to_string());
-    request.push_str(&format!("{}{}", "\r\n\r\n", body));
+    // request.push_str(&body.len().to_string());
+    request.push_str("\r\n\r\n");
+    // request.push_str(&format!("{}{}", "\r\n\r\n", body));
     request
 }
 
 fn main() {
-    let endpoint = "127.0.0.1:5862";
+    let endpoint = "127.0.0.1:3000";
     let mut stream = TcpStream::connect(endpoint).unwrap();
-    let res = stream.write(create_normal_get("/", "hello world").as_bytes()).unwrap();
+    stream.write(create_normal_get("/", "hello world").as_bytes()).unwrap();
+    let res = stream.read(&mut buffer)?
     stream.flush().unwrap();
     println!("{:?}", res);
 }
