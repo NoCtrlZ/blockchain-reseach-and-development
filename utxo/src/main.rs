17ba658781051b15a0883d8af48a894a513bdca8
NoCtrlZ <phantomofrotten@gmail.com>
2020-03-23 11:38:55 +0900

    change structure

--- utxo/src/main.rs
+++ utxo/src/main.rs
@@ -1,10 +1,10 @@
 mod utxo;
 
-use utxo::Transaction;
+use utxo::Utxo;
 
 fn main() {
     let dummy_address = "0x114514";
-    let mut utxo = Transaction::new();
+    let mut utxo = Utxo::new();
     let tx_hash = utxo.admin_transfer(dummy_address);
     println!("tx hash is {}", &tx_hash);
     println!("{:?}", utxo);
--- utxo/src/utxo.rs
+++ utxo/src/utxo.rs
@@ -4,12 +4,18 @@ use crypto::sha2::Sha256;
 use crypto::digest::Digest;
 
 #[derive(Debug, Deserialize, Serialize)]
+pub struct Utxo {
+    transactions: Vec<Transaction>
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Transaction {
+    is_spent: bool,
     input: Vec<Input>,
     output: Vec<Output>
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 struct Input {
     transaction: Transaction,
     output_index: u8
@@ -21,21 +27,35 @@ struct Output {
     amount: u64
 }
 
-impl Transaction {
-    pub fn new() -> Transaction {
-        Transaction {
-            input: Vec::new(),
-            output: Vec::new()
+impl Utxo {
+    pub fn new() -> Utxo {
+        Utxo {
+            transactions: Vec::new()
         }
     }
 
     pub fn admin_transfer(&mut self, recipient: &str) -> String {
+        let input = Input {
+            transaction: Transaction {
+                is_spent: true,
+                input: Vec::new(),
+                output: Vec::new()
+            },
+            output_index: 0
+        };
         let output = Output {
             recipient: recipient.to_string(),
             amount: 100
         };
-        self.output.push(output.clone());
-        transactions_hash(&json!(output).to_string())
+        let mut transaction = Transaction {
+            is_spent: false,
+            input: Vec::new(),
+            output: Vec::new()
+        };
+        transaction.input.push(input);
+        transaction.output.push(output);
+        self.transactions.push(transaction.clone());
+        transactions_hash(&json!(transaction).to_string())
     }
 
     // pub fn transfer(&mut self, sender: &str, recipient: &str, amount) {
