b1222042140e24d4cbe6e4b68cf3635ad6385754
NoCtrlZ <phantomofrotten@gmail.com>
2020-03-25 13:31:19 +0900

    cli init

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,18 @@
+extern crate clap;
+
+use clap::{Arg, App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("sexydynamite")
+        .version("0.1.0")
+        .author("NoCtrlZ <phantomofrotten@gmail.com>")
+        .about("Test Blockchain Server")
+        .subcommand(SubCommand::with_name("init")
+            .about("init project")
+        );
+    let matches = app.get_matches();
+
+    if let Some(ref matches) = matches.subcommand_matches("init") {
+        println!("init wallet");
+    }
 }
